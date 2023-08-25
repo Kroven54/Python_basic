ebd321050f83ee7ff327aec6681d4b304177bed1
Kroven54 <Kroven90@mail.ru>
2023-08-25 12:06:38 +0700

    Переписал решение задачи на более локоничное

--- test.py
+++ test.py
@@ -1,16 +1,12 @@
 read_file: str = 'first_file.txt'
 write_file: str = 'second_file.txt'
-with open(read_file, 'r', encoding='utf-8') as fr, open(write_file, 'w', encoding='utf-8') as fw:
+with open(read_file, 'r', encoding='utf-8') as fr, open(write_file, 'w+', encoding='utf-8') as fw:
     for line in fr:
         line = line[::-1]
         fw.write(line)
-
-f = open(read_file, 'r', encoding='utf-8')
-first = f.read()
-print(first)
-f.close()
-
-s = open(write_file, 'r', encoding='utf-8')
-second = s.read()
-print(second)
-s.close()
+    fr.seek(0)
+    first = fr.read()
+    print(first)
+    fw.seek(0)
+    second = fw.read()
+    print(second)
